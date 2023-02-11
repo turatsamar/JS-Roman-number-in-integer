2a6f7a6bba3450b76ddea415d1cb98bd6028aa0f
TIMA <64804931+turatsamar@users.noreply.github.com>
2023-02-12 04:31:52 +0900

    Add files via upload

---
+++ twoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function (nums, target) {
+    const mp = {}
+ 
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i]
+    
+        if (diff in mp) return [i, mp[diff]]
+ 
+        mp[nums[i]] = i
+    }
+ }
